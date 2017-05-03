73fbc5389079a1289a55549dc651daa4da161395
MichaelCook <michael@evoluted.net>
2017-05-03 13:56:42 +0100

    Better sass compilation (node-sass) and file structure

--- app/js/renderer.js
+++ app/js/renderer.js
@@ -1,28 +1,8 @@
-
-/*
-import 'core-js/fn/object/assign';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/app';
-*/
+import App from '../components/app';
 
-//const ReactDOM = require('react-dom');
-//const React = require('react');
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-//window.React = React;
-
-import App from '../components/app.jsx';
-
-/*
 ReactDOM.render(
   <App />,
   document.getElementById('app')
 );
-*/
-
-window.onload = function(){
-  ReactDOM.render(<App />, document.getElementById('app'));
-}
